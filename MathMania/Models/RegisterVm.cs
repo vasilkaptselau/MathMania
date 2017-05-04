40f7e36f43f2f1188e7dcc1a069903cfa6070cf4
Pete Anthis <anthisp@gmail.com>
2017-05-04 09:28:01 -0700

    Redo'ing login page

--- MathMania/Models/RegisterVm.cs
+++ MathMania/Models/RegisterVm.cs
@@ -6,18 +6,35 @@ namespace MathMania.Models
     public class RegisterVm
     {
         [Key]
-        [Display(Name = "UserName")]
-        public string UserName { get; set; }
+        public int UserID { get; set; }
 
-        [Required]
-        [Display(Name = "Password")]
-        [MaxLength(8)]
-        public string Password { get; set;}
+        [Required(ErrorMessage = "Please enter the Students first name")]
+        public string FirstName { get; set; }
 
-        [Required]
-        [Display(Name = "Confirm Password")]
-        [MaxLength(8)]
+        [Required(ErrorMessage ="Please enter the Students last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please create a Login for the student.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        public string Password { get; set; }
+
         public string ConfirmPassword { get; set; }
+
+        //[Key]
+        //[Display(Name = "UserName")]
+        //public string UserName { get; set; }
+
+        //[Required]
+        //[Display(Name = "Password")]
+        //[MaxLength(8)]
+        //public string Password { get; set;}
+
+        //[Required]
+        //[Display(Name = "Confirm Password")]
+        //[MaxLength(8)]
+        //public string ConfirmPassword { get; set; }
     }
 
 
