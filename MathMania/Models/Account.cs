7ee722df56ed441c97d4705449143f4ebe7c23b0
Vasil Kaptselau <kopvasya@gmail.com>
2017-05-13 22:29:10 -0700

    Last version - release 1

---
+++ MathMania/Models/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MathMania.Models
+{
+    public class Account
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Students first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage ="Please enter the Students last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please create a Login for the student.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+
+
+
+
+
+}
--- MathMania/Models/RegisterVm.cs
+++ MathMania/Models/RegisterVm.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MathMania.Models
 {
-    public class UserAccount
+    public class RegisterVm
     {
         [Key]
         public int UserID { get; set; }
@@ -11,7 +11,7 @@ namespace MathMania.Models
         [Required(ErrorMessage = "Please enter the Students first name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage ="Please enter the Students last name")]
+        [Required(ErrorMessage = "Please enter the Students last name")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Please create a Login for the student.")]
